f5ef3e71aba618b84c5d5616b2c7801fe1b7fa11
SitiMaiSarah980 <32213515+SitiMaiSarah980@users.noreply.github.com>
2018-05-03 16:45:43 +0800

    Membuat attribut dan behavior

--- src/praktek02triangle/Triangle.java
+++ src/praktek02triangle/Triangle.java
@@ -1,4 +1,14 @@
 package praktek02triangle;
 public class Triangle {
+    double sisi1;
+    double sisi2;
+    double sisi3;
     
+    void cetakInfo(){
+        System.out.println("========================");
+        System.out.println("Sisi 1 : "+sisi1);
+        System.out.println("Sisi 2 : "+sisi2);
+        System.out.println("Sisi 3 : "+sisi3);
+        System.out.println("========================");
+    }
 }
